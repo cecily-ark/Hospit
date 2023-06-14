3121d9e07938eb762f9e2b0f2787d82701578248
jachvictor <chukwujachike175@gmail.com>
2023-06-14 21:13:03 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+
+function App() {
+  return (
+    <div className="App">
+      <section className="for-sidebar">
+       
+      </section>
+      <section className="for-dashboard">
+      <Dashboard head="Dashboard" />
+      </section>
+    </div>
+  );
+}
+
+export default App;
